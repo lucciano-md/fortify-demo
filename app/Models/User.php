f19311a6c0bb67b6a0d94872d3aaa9c84269b65a
Luciano Andrade <luciano@miracledevs.com>
2023-10-05 11:24:41 +0000

    installer fortifyUI

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -31,6 +33,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+	'two_factor_recovery_codes',
+	'two_factor_secret',
     ];
 
     /**
